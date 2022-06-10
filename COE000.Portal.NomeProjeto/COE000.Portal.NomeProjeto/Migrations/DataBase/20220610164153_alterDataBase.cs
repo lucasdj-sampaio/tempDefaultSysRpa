932bfa66f093fd20bb6505eba9dbec67d89164a9
Lucas de Jesus Sampaio <lucas.sampaio@br.atento.com>
2022-06-10 13:43:26 -0300

    Ajuste migration

---
+++ COE000.Portal.NomeProjeto/COE000.Portal.NomeProjeto/Migrations/DataBase/20220610164153_alterDataBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace COE000.Portal.NomeProjeto.Migrations.DataBase
+{
+    public partial class alterDataBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOn",
+                table: "TB_UserConfig",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateOn",
+                table: "TB_UserConfig");
+        }
+    }
+}
